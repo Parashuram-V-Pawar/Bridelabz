c9c630ee0d3b029275be7c19570f36d7a89a5c66
Parashuram <parashurampawar56@gmail.com>
2025-12-20 10:27:10 +0530

    modify program: [Parashuram] leap year

--- src/math/leap_year.py
+++ src/math/leap_year.py
@@ -1,6 +1,8 @@
-year = int(input("Enter the year: "))
-while(len(str(year))<4) :
+year = input("Enter the year: ")
+while(len(year)<4) :
   print("please enter 4 digit year")
+
+year = int(year)
 if((year%4 == 0 and year%100 !=0) or year%400 == 0):
   print(f"{year} is a leap year")
 else: 
