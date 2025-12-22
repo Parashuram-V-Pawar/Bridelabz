640aafacb2bd287bd1d69a0f00f86da05abba464
Parashuram <parashurampawar56@gmail.com>
2025-12-22 11:54:11 +0530

    modified program: [Parashuram] armstrong number checker

--- src/practice_programs_week1/armstrong_number_checker.py
+++ src/practice_programs_week1/armstrong_number_checker.py
@@ -13,13 +13,13 @@
 # NO        # otherwise
 #
 
-# This function is used to check armstrong number if the digits are more than 3 or less than 3
-# def no_of_digits(num) :
-#   count = 0
-#   while num > 0:
-#     num = num // 10
-#     count += 1
-#   return count
+# This function is used to count number of digits in the number
+def no_of_digits(num) :
+  count = 0
+  while num > 0:
+    num = num // 10
+    count += 1
+  return count
 
 # Function to check for armstrong number
 def armstrong_number_checker(number):
@@ -31,21 +31,25 @@ def armstrong_number_checker(number):
     # First we are extracting last digit by using modulo operator 
     digit = num % 10
     # We are calculating cube of the digit extracted and adding it to the armstrong number
-    armstrong = armstrong + (digit**3)
+    armstrong = armstrong + (digit ** (no_of_digits(number)))
     # Performing integer division to remove last digit from the number
     num = num // 10
   # Once all the operations are done checking if number received and armstrong number has same value.
   # If the number is less than 10 by default it is considered as Armstrong number.
   # If yes print "YES" else print "NO"
-  if number == armstrong or number < 10:
+  if number == armstrong:
     print("YES")
   else:
     print("No")
 
 # Input statement, takes positive integer values as input
-number = int(input("Enter a integer: "))
+number = int(input("Enter a 3 digit integer: "))
 # if the inout is negative prints error message else calls the function to check armstrong number
 if number < 0:
   print("Negative numbers are not armstrong numbers.\nPlease enter positive number")
+elif number > 0 and number < 10:
+  print("YES")
+elif number > 9 and number < 100:
+  print("There ar no Armstrong numbers for 2 digit numbers")
 else:
   armstrong_number_checker(number)
